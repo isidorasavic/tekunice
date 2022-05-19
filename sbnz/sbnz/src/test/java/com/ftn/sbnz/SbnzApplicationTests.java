fc18cc7fb04da402121c01855e9c11dc3a4f1f9f
isidorasavic <isidora99savic@gmail.com>
2022-05-19 20:14:25 +0200

    initial project

---
+++ sbnz/sbnz/src/test/java/com/ftn/sbnz/SbnzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.sbnz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbnzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
